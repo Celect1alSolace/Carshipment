c817a900f8dc61f75eed56ec6555b38ab53cbda6
Celest1alSolace <anvarjonov.m777@gmail.com>
2024-07-08 00:19:59 +0500

    Plus Animation Added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var elModeBtn = document.querySelector(".night-img");
+let elDropDownHead = document.querySelector(".questions__svg");
+let elDropDownHideContent = document.querySelector(".questions__hide-content");
 
 elModeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
 
-
+elDropDownHead.addEventListener("click", function () {
+  elDropDownHideContent.classList.toggle("show");
+});
